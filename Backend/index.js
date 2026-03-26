d75276e620a245a82bcb0677e24ac623941c52e8
RajatBisht-dev <rajatbisht1947@gmail.com>
2026-03-26 12:07:54 +0530

    with database

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,12 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json());
 
 dotenv.config();
